6fab2891dac3af72791b16cfb28e1fc1584fc479
Alex Curnow <alex@curnows.com>
2024-09-26 14:51:33 +0000

    DEV: added dashboard app and profile html

--- project/models.py
+++ project/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
-from todo.models import Task
 
 # Create your models here.
 class Project(models.Model):
--- tasknest/urls.py
+++ tasknest/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from dashboard import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todo.urls')),
     path('accounts/', include('allauth.urls')),
+    path('accounts/profile/', views.profile, name='profile'),
 ]
