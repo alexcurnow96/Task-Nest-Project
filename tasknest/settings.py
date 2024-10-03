216cfbf1796da16499f91445a855f3eaadccb287
Alex Curnow <alex@curnows.com>
2024-10-03 10:13:27 +0000

    DOC: Set debug to false

--- tasknest/settings.py
+++ tasknest/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-alexcurnow9-portfoliopr-llpzmw5yf7l.ws.codeinstitute-ide.net', '.herokuapp.com']
